9ab661e7294c3ad7f23d9e0f2d3d7550a18ba08f
amratef84 <ammeratef84@gmail.com>
2024-09-10 21:08:51 +0530

    fix bugs

--- tech_committee/tech_committee/doctype/tc_meeting/tc_meeting.js
+++ tech_committee/tech_committee/doctype/tc_meeting/tc_meeting.js
@@ -2,7 +2,17 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('TC Meeting', {
-	 refresh: function(frm) {
+	onload: function(frm) {
+		frm.set_query('subjects_committee', () => {
+			return {
+				filters: {
+					subject_proirty: ''
+				}
+			}
+		})
+    }
+	//subjects_committee:
+	// refresh: function(frm) {
 
-	 }
+	// }
 });
