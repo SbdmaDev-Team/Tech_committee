a4fb68414c96bd1db41a5ae8e4175b895065ae49
MohamedAbdulsalam96 <moha2016it@gmail.com>
2024-09-10 21:55:11 +0530

    fix Subject Committe Request

--- tech_committee/tech_committee/doctype/subject_committe_request/subject_committe_request.js
+++ tech_committee/tech_committee/doctype/subject_committe_request/subject_committe_request.js
@@ -1,8 +1,5 @@
-frappe.ui.form.on('Subject Committee Request', {
-    party_type: function(frm) {
-        // Clear the Party field when Party Type is changed
-        frm.set_value('party', null);
-        // Trigger a refresh to make sure the field is updated
-        frm.refresh_field('party');
+frappe.ui.form.on('Subject Committe Request', {
+    patry_type: function(frm) {
+        frm.set_value('party', '' )
     }
 });
